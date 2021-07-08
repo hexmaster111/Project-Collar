9a2a90807475d2ef497e881999466b99e87aed0d
Hailey <Hexmaster111@gmail.com>
2021-07-07 19:22:12 -0500

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,83 @@
+
+/********** Genral config**************/
+
+// Uncomment one DST changeover rule:
+#define USA_DST
+//#define EU_DST
+
+//-6 for central time zone
+#define timeZoneTimeCorrection 6
+
+//uncomment to enable icon instend of sending battery voltage
+#define BATT_ICON
+
+// Assined ID
+#define DRONE_ID 0000
+
+//Uncomment to enable dev messages
+#define DEV_BUILD
+
+// Use to change the status led pin
+#define LED_BUILTIN 14
+
+//Use to define a shock pin
+#define SHOCK_PIN 2 
+
+#define VIBRATION_PIN 14
+
+//time between shocks in ms 
+#define SHOCK_WAIT_TIME 5000
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME "IOUSERNAME"
+#define IO_KEY "aio_fjdklsajfdklsajfijeioawjfio or something, put your key here"
+
+/******************************* WIFI **************************************/
+#define WIFI_SSID "NETWORK NAME"
+#define WIFI_PASS "NETWORK PASSWORD"
+
+// uncomment the following line if you are using airlift
+// #define USE_AIRLIFT
+
+// uncomment the following line if you are using winc1500
+// #define USE_WINC1500
+
+// comment out the following lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
+    defined(ADAFRUIT_PYPORTAL)
+// Configure the pins used for the ESP32 connection
+#if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+// Don't change the names of these #define's! they match the variant ones
+#define SPIWIFI SPI
+#define SPIWIFI_SS 10 // Chip select pin
+#define NINA_ACK 9    // a.k.a BUSY or READY pin
+#define NINA_RESETN 6 // Reset pin
+#define NINA_GPIO0 -1 // Not connected
+#endif
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
+                   NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
+#else
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
+/******************************* FONA **************************************/
+
+// the AdafruitIO_FONA client will work with the following boards:
+//   - Feather 32u4 FONA -> https://www.adafruit.com/product/3027
+
+// uncomment the following two lines for 32u4 FONA,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_FONA.h"
+// AdafruitIO_FONA io(IO_USERNAME, IO_KEY);
+
+/**************************** ETHERNET ************************************/
+
+// the AdafruitIO_Ethernet client will work with the following boards:
+//   - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
+
+// uncomment the following two lines for ethernet,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_Ethernet.h"
+// AdafruitIO_Ethernet io(IO_USERNAME, IO_KEY);
